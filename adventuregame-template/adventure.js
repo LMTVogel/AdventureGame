fec502419b102dec6c96872515b40dd1bfb3e8f9
Luuk Vogel <99051050@mydavinci.nl>
2018-12-26 11:09:28 +0100

    Final Update

--- adventuregame-template/adventure.js
+++ adventuregame-template/adventure.js
@@ -22,6 +22,9 @@ alert("Voor de beste ervaring moet u de muziek aanzetten. Klik op de muziek knop
 
 function startscreen() {
 	console.log("Start Screen");
+	gameImage=document.createElement("img");
+	gameContainer.insertBefore(gameImage, gameContainer.childNodes[2]);
+	gameImage.src="images/goeie.png";
 	title.innerHTML="Cave Game";
 	button3.onclick=function() {music();};
 	button3.innerHTML="Muziek";
@@ -60,7 +63,8 @@ function itemAlert() {
 
 function winNoDiamants() {
 	console.log("Win without diamants");
-	title.innerHTML="YOU WIN! YOU ROCK LIKE QUEEN!!!! But you're poor because you didn't took the diamants."
+	gameImage.src="images/win.png";
+	title.innerHTML="YOU WIN! But you're poor because you didn't took the diamants."
 	description.innerHTML="Well done.";
 	button2.style.display="none";
 	button3.style.display="none";
@@ -71,7 +75,8 @@ function winNoDiamants() {
 
 function winDiamants() {
 	console.log("Win with diamants");
-	title.innerHTML="YOU WIN! YOU ROCK LIKE QUEEN!!!! And you have a lot of money. Like the members of Queen.";
+	gameImage.src="images/win.png";
+	title.innerHTML="YOU WIN! And you have a lot of money.";
 	description.innerHTML="Well done.";
 	button2.style.display="none";
 	button3.style.display="none";
@@ -82,6 +87,7 @@ function winDiamants() {
 
 function level1() {
 	console.log("Level 1");
+	gameImage.src="images/goeie.png"
 	title.innerHTML="De heuvel";
 	description.innerHTML="Je spawned in een prachtige vierkante blok achtige wereld. Je moet gaan overleven. Je ziet een heuvel. Ga je hierop je huis bouwen om te overleven of doe je dit niet.";
 	button2.onclick=function() {
@@ -96,6 +102,7 @@ function level1() {
 
 function level2() {
 	console.log("Level 2");
+	gameImage.src="images/hole.png"
 	title.innerHTML="Je hebt het huis gebouwd.";
 	description.innerHTML="Je bouwt een huis. Maar je ziet een gat. Ga je het gat in om te kijken wat er zit of maak je het gat dicht om je huis te bouwen?";
 	button2.innerHTML="Gat dicht maken.";
@@ -110,6 +117,7 @@ function level2() {
 
 function level3() {
 	console.log("Level 3");
+	gameImage.src="images/cave.jpeg"
 	title.innerHTML="De grot";
 	description.innerHTML="Je komt erachter dat het een cave is. Je loopt verder en je valt in een put die recht naar beneden gaat. Je valt in het water. Steek je een fakkel aan om jezelf droog te maken en jezelf te oriënteren of pak je geen fakkel?";
 	button2.innerHTML="Je pakt geen fakkel.";
@@ -124,6 +132,7 @@ function level3() {
 
 function level4() {
 	console.log("Level 4");
+	gameImage.src="images/cavehouse.jpg"
 	title.innerHTML="De ontdekking";
 	description.innerHTML="Je ziet dat de cave enorm is met prachtige onderwaterrivieren. Je gaat op onderzoek uit. Je ziet verderop licht branden. Je gaat erop af. Er blijkt een heks te wonen. Maak je haar dood of vraag je wat ze hier doet?";
 	button2.innerHTML="Je gaat met de heks praten";
@@ -137,6 +146,7 @@ function level4() {
 
 function level5() {
 	console.log("Level 5");
+	gameImage.src="images/bottle.jpg"
 	title.innerHTML="Het huis doorzoeken";
 	description.innerHTML="Je hebt haar dood gemaakt en je doorzoekt het huis voor overleving spullen. Op de zolder van het huis vind je een flesje met water dat glinstert en het geeft licht. Je ziet ook een zwaard. Je moet zelf weten of je hem pakt. Drink je het of laat je het liggen.";
 	button1.innerHTML="Pak het zwaard op.";
@@ -158,6 +168,7 @@ function level5() {
 
 function level6() {
 	console.log("Level 6");
+	gameImage.src="images/skelet.png"
 	title.innerHTML="De grot verder onderzoeken";
 	description.innerHTML="Je gaat verder op onderzoek in de grot. Je komt een groep skeletten tegen. Ren je weg of maak je ze dood? Het word ook steeds donkerder in de grot. Je zult fakkels nodig moeten hebben. ";
 	button1.innerHTML="Pak fakkel";
@@ -185,6 +196,7 @@ function level6() {
 
 function level7() {
 	console.log("Level 7");
+	gameImage.src="images/mijn.jpeg"
 	title.innerHTML="Groot labyrint";
 	description.innerHTML="Je bent er inmiddels achter gekomen dat het een gestoord groot labyrint is. Je weet dat het heel moeilijk gaat worden om weer omhoog te komen.  Je moet dus opzoek naar materialen. Je ziet een oude mijn. Je loopt er op af. Het word erg donker nu je verder gaat.";
 	button1.style.display="none";
@@ -206,6 +218,7 @@ function level7() {
 
 function level8() {
 	console.log("Level 8");
+	gameImage.src="images/boom.jpeg"
 	title.innerHTML="Je gaat de mijn in";
 	description.innerHTML="Je ziet hout in de mijn liggen. Pak je het hout op?";
 	button1.style.display="none";
@@ -221,6 +234,7 @@ function level8() {
 
 function level9() {
 	console.log("Level 9");
+	gameImage.src="images/pickaxe.png"
 	title.innerHTML="Werkbank maken";
 	description.innerHTML="Je hebt het hout opgepakt. Nu kun je een werkbank maken. Maak je een schep of maak je een pikhouweel.";
 	button1.style.display="none";
@@ -236,6 +250,7 @@ function level9() {
 
 function level10() {
 	console.log("Level 10");
+	gameImage.src="images/iron.jpeg"
 	title.innerHTML="IJzer of diamant?";
 	description.innerHTML="Doordat je nu een pikhouweel hebt kun je naar ijzer en diamant zoeken om betere tools te maken want met een houten pikhouweel kom je niet naar het oppervlak van de aarde. Je komt bij een plek waar veel ijzer zit. Ga je dit minen of niet?";
 	button1.style.display="none";
@@ -251,6 +266,7 @@ function level10() {
 
 function level11() {
 	console.log("Level 11");
+	gameImage.src="images/coal.jpg"
 	title.innerHTML="Pikhouweel van ijzer";
 	description.innerHTML="Je kunt nu een pikhouweel maken die van ijzer is gemaakt. Je gaat opzoek naar diamant want dit kun je nu minen met je ijzeren pikhouweel. Maar plots kom je erachter dat je fakkels op zijn. Je hebt kool nodig om nieuwe fakkels te maken of je gaat verder in het donker.";
 	button1.style.display="none";
@@ -266,6 +282,7 @@ function level11() {
 
 function level12() {
 	console.log("Level 12");
+	gameImage.src="images/diamant.png"
 	title.innerHTML="Op naar het daglicht!";
 	description.innerHTML="Je kunt weer verder om te zoeken naar diamanten. Je vindt er toevallig een paar en nu kun je omhoog gaan graven naar het daglicht. Je mined en mined zonder enige slaap. Je hoort een zombie boven je. Je weet dus dat er een holle ruimte zit. Maar je moet er toch doorheen minen. Ga je er doorheen of er onderdoor. ";
 	button1.style.display="none";
@@ -281,6 +298,7 @@ function level12() {
 
 function level13() {
 	console.log("Level 13");
+	gameImage.src="images/more.jpg"
 	title.innerHTML="Je gaat door de holle ruimte!";
 	description.innerHTML="Je maakt de zombie dood en je gaat verder. Je mined verder naar boven. Je komt diamanten tegen. Neem je deze mee?";
 	button1.style.display="none";
